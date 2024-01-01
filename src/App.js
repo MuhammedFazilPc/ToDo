291e73c5124808bcd9ec54605188482c9f158eb8
fazil <muhammedfazilpc@gmail.com>
2024-01-01 20:01:30 +0530

    add and delete toDos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [ToDos, SetToDos] = useState([]);
+  const [ToDo, SetToDo] = useState("");
+  // const [date, setDate] = useState("")
+  // const [ischecked,setChecked]=useState(false);
+
+  const day = new Date().getDay();
+
+  // Convert the day of the week to a string
+  const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const todayString = daysOfWeek[day];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's {todayString} 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input value={ToDo} onChange={(e) => SetToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={() => { SetToDos([...ToDos, { text: ToDo, status: false, id: Date.now() }]); SetToDo("") }} className="fas fa-plus"></i>
+
+      </div>
+
+      <div className="todos">
+        {
+          ToDos.map((todo, index) =>
+          (
+            <div className="todo" >
+              <div className="left">
+                <input onChange={(e) => {
+                  SetToDos(ToDos.filter(obj2 => {
+                    if (obj2.id === todo.id)
+                      obj2.status = e.target.checked;
+                    return obj2;
+
+                  }))
+                }} type="checkbox" name="" id="" />
+                <p>{todo.text}</p>
+              </div>
+              <div className="right">
+              </div>
+            </div>
+          )
+          )
+        }
+
+        <div className="subHeading">
+          <br />
+          <h2>Completed Tasks 💪 </h2>
+        </div>
+        {
+          ToDos.map((todo) => {
+            if (todo.status) {
+              return (
+                <div className='input'>
+                  <input type='text' placeholder={todo.text}></input>
+                </div>
+              )
+            }
+            return null;
+          }
+
+          )
+        }
+      </div>
+
+
+    </div >
   );
 }
 
